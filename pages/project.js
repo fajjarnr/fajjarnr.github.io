16f423e065e7582e402e88d4bbe7621419c951f1
fajjarnr <zfajart@gmail.com>
2021-09-20 10:00:18 +0700

    push

--- pages/project.js
+++ pages/project.js
@@ -23,8 +23,7 @@ export default function Project() {
               Project
             </h2>
             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
-              libero labore natus atque, ducimus sed.
+              List All My Project
             </p>
           </div>
           <div className="mt-12 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-3">
