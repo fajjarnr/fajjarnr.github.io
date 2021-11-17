2e253317e0c13ddaf011ab162b0248d94e3e160f
fajjarnr <zfajart@gmail.com>
2021-11-17 17:43:59 +0700

    google-site-verification

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
+import Head from 'next/head';
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
@@ -9,6 +9,10 @@ export default function Home() {
         <title>Fajar Nur Rohman</title>
         <meta name="description" content="Portofolio Fajar Nur Rohman" />
         <link rel="icon" href="/logo.svg" />
+        <meta
+          name="google-site-verification"
+          content="i5XC8tWxae3ocQwlybiLBod5yysgz-kX5Nv5fJbwNQo"
+        />
       </Head>
 
       <main>
