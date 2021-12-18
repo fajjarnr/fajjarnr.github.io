47f171d251dfa092d1988754d208a4e8faa15b1f
fajjarnr <zfajart@gmail.com>
2021-12-18 12:05:27 +0700

    meta tags

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,15 @@ export default function Home() {
           httpEquiv="Content-Type"
           content="text/html; charset=ISO-8859-1"
         />
-        <meta property="og:title" content="Jual Beli" />
+        <meta name="title" content="Fajar Portofolio" />
         <meta name="description" content="Portofolio Fajar Nur Rohman" />
+        <meta property="og:title" content="Fajar Portofolio" />
+        <meta property="og:description" content="Portofolio Fajar Nur Rohman" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.fajar.tech" />
         <meta property="og:description" content="Portofolio Fajar Nur Rohman" />
         <meta property="og:determiner" content="the" />
-        <meta property="og:locale" content="en_US" />
+        <meta property="og:locale" content="id_ID" />
         <meta property="og:site_name" content="Jual Beli" />
         <meta property="og:image" content="https://www.fajar.tech/logo.svg" />
         <meta
@@ -34,6 +36,17 @@ export default function Home() {
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="300" />
         <meta property="og:image:alt" content="logo" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.fajar.tech/" />
+        <meta property="twitter:title" content="Fajar Portofolio" />
+        <meta
+          property="twitter:description"
+          content="Portofolio Fajar Nur Rohman"
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.fajar.tech/logo.svg"
+        />
       </Head>
 
       <main>
