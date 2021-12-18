dccecf49961eec8eb03b0949a39bce5a905347c7
fajjarnr <zfajart@gmail.com>
2021-12-18 16:39:22 +0700

    title

--- components/Title.js
+++ components/Title.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Title({ name }) {
   return (
     <Head>
-      <title>{name} ~ Fajar Nur Rohman</title>
+      <title>{name} ~ Fajar Nur Rohman 👋</title>
     </Head>
   );
 }
