1614440cfcf0dc8f51ca132e6fd11cb450655cd7
fajjarnr <zfajart@gmail.com>
2021-12-18 18:37:45 +0700

    styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import Router from "next/router";
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
-import "tailwindcss/tailwind.css";
-import "../styles/globals.css";
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import 'tailwindcss/tailwind.css';
 
-NProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+NProgress.configure({
+  showSpinner: false,
+  template:
+    '<div class="bar" role="bar"></div><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>',
+});
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
